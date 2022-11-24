bc53a9dc6410bf9d38a7a9bcf26750498509ae0b
Hortense ROBIN <hrobin@e1r7p2.clusters.42paris.fr>
2022-11-24 09:10:21 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: hrobin <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/21 13:51:57 by hrobin            #+#    #+#             */
-/*   Updated: 2022/11/22 16:59:00 by hrobin           ###   ########.fr       */
+/*   Updated: 2022/11/24 08:45:02 by hrobin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,26 +17,26 @@ char	*ft_keeprest_str(int fd, char *rest_str)
 	char	*buff;
 	int		nb_bytes;
 
-	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);	//je sais pas pour le +1 (pour le \0 jpense)
+	buff = malloc(sizeof(char) * BUFFER_SIZE + 2);
 	if (!buff)
 		return (NULL);
 	nb_bytes = 1;
-	while (!ft_strchr(rest_str, '\n') && nb_bytes != 0) // tant que il n'y a pas de \n etr que le retour de read est pas -1
+	while (!ft_strchr(rest_str, '\n') && nb_bytes != 0)
 	{
 		nb_bytes = read(fd, buff, BUFFER_SIZE);
-		if (nb_bytes == -1) //Si read renvoi -1 c'est quand il y a une erreur 
+		if (nb_bytes == -1)
 		{
 			free(buff);
 			return (NULL);
 		}
-		buff[nb_bytes] = '\0'; // ici
-		rest_str = ft_strjoin(rest_str, buff); // concat au cas ou rest_str ya qqchose
+		buff[nb_bytes] = '\0';
+		rest_str = ft_strjoin(rest_str, buff);
 	}
 	free (buff);
 	return (rest_str);
 }
 
-char	*ft_findline(char *rest_str) // verif et renvoi jusquau '\n'
+char	*ft_findline(char *rest_str)
 {
 	int	i;
 	char	*newline;
@@ -46,7 +46,7 @@ char	*ft_findline(char *rest_str) // verif et renvoi jusquau '\n'
 		return (NULL);
 	while (rest_str[i] != '\n' && rest_str[i])
 		i++;
-	newline = (char *)malloc(sizeof(char) * (i + 1)); // je sais toujours pas pour le \0
+	newline = (char *)malloc(sizeof(char) * (i + 2));
 	if (!newline)
 		return (NULL);
 	i = 0;
@@ -55,7 +55,7 @@ char	*ft_findline(char *rest_str) // verif et renvoi jusquau '\n'
 		newline[i] = rest_str[i];
 		i++;
 	}
-	if (rest_str[i] == '\n') // recup le \n vu qu'on veut le renvoyer
+	if (rest_str[i] == '\n')
 	{
 		newline[i] = rest_str[i];
 		i++;
@@ -64,7 +64,7 @@ char	*ft_findline(char *rest_str) // verif et renvoi jusquau '\n'
 	return (newline);
 }
 
-char	*ft_newstr(char *rest_str) // recup le reste pour une nouvelle chaine 
+char	*ft_newstr(char *rest_str)
 {
 	int	i;
 	int	j;
@@ -78,7 +78,7 @@ char	*ft_newstr(char *rest_str) // recup le reste pour une nouvelle chaine
 		free(rest_str);
 		return (NULL);
 	}
-	str = malloc(sizeof(char) * i + 1);
+	str = (char *)malloc(sizeof(char) * (ft_strlen(rest_str) - i + 1));
 	if (!str)
 		return (NULL);
 	j = 0;
@@ -104,3 +104,27 @@ char	*get_next_line(int fd)
 	rest_str = ft_newstr(rest_str); // on recup si ya qqchose apres le /n
 	return (line);
 }
+/*
+int main()
+{
+     int fd; // file descriptor
+     char *line; // ligne a lire
+
+     fd = open("blabla.txt", O_RDONLY); // on ouvre notre fichier en read only
+     while(1)
+     {
+         line = get_next_line(fd); // on lit la ligne de notre fichier ouvert et on la stock dans line
+         if (line == NULL) // si line est null, on a atteint la fin du fichier car get_next_line renvoie NULL quand il n'y a plus rien a lire
+         {
+             free(line); // on libere la memoire allouee a line pour eviter les leaks car on aura plus aucunn moyen de la free si on le fait pas mtn
+             break; // on sort de la boucle
+         }
+         else // si line n'est pas null, on a pas atteint la fin du fichier donc on affiche la ligne
+         {
+             printf("%s", line); // on affiche la ligne
+             free(line); // on libere la memoire allouee a line pour eviter les leaks car on aura plus aucunn moyen de la free si on le fait pas mtn
+         }
+     }
+     return (0);
+ }
+*/
